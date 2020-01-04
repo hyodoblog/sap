d7c710ed9668a07739687f6737e61b34527db254
yusei-hyodo <yusei.hyodo@gmail.com>
2020-01-04 22:59:52 +0900

    init notifications

--- app/controllers/admin/init_controller.rb
+++ app/controllers/admin/init_controller.rb
@@ -1,2 +1,36 @@
-class Admin::InitController < ApplicationController
+class Admin::InitController < Admin::ApplicationController
+  def all
+    laboratories_destroy
+    students_destroy
+    flash[:notice] = '全学生・研究室を削除しました'
+    redirect_to admin_mypage_path
+  end
+
+  def laboratory
+    laboratories_destroy
+    flash[:notice] = '全研究室を削除しました'
+    redirect_to admin_mypage_path
+  end
+
+  def student
+    students_destroy
+    flash[:notice] = '全学生を削除しました'
+    redirect_to admin_mypage_path
+  end
+
+  private
+
+  def laboratories_destroy
+    laboratories = Laboratory.where(user_id: current_user.id)
+    laboratories.each do |laboratory|
+      laboratory.destroy
+    end
+  end
+
+  def students_destroy
+    students = Student.where(user_id: current_user.id)
+    students.each do |student|
+      student.destroy
+    end
+  end
 end
