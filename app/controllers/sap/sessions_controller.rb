03e1fa37d4362dd25d9432a8c2c7131e8a6c9f6b
yusei-hyodo <yusei.hyodo@gmail.com>
2020-01-18 18:46:07 +0900

    admin view edit

--- app/controllers/sap/sessions_controller.rb
+++ app/controllers/sap/sessions_controller.rb
@@ -5,11 +5,15 @@ class Sap::SessionsController < Sap::ApplicationController
   end
 
   def create
+    if user_signed_in?
+      flash[:alert] = '管理者アカウントを一度ログアウトしてください！'
+      redirect_back(fallback_location: root_path) and return
+    end
     admin_id = Config.find_by(sap_key: params[:sap_key]).user_id
     # 研究室
-    laboratory = Laboratory.where(user_id: admin_id).where(loginid: params[:sap_session][:loginid])
+    laboratory = Laboratory.where(user_id: admin_id).where(loginid: login_params[:loginid])
     laboratory = laboratory[0]
-    if laboratory && laboratory.authenticate(params[:sap_session][:password])
+    if laboratory && laboratory.authenticate(login_params[:password])
       session[:laboratory_id] = laboratory.id
       session[:sap_key] = params[:sap_key]
       flash[:notice] = 'ログインしました'
@@ -17,9 +21,9 @@ class Sap::SessionsController < Sap::ApplicationController
     end
 
     # 学生
-    student = Student.where(user_id: admin_id).where(loginid: params[:sap_session][:loginid])
+    student = Student.where(user_id: admin_id).where(loginid: login_params[:loginid])
     student = student[0]
-    if student && student.authenticate(params[:sap_session][:password])
+    if student && student.authenticate(login_params[:password])
       session[:student_id] = student.id
       session[:sap_key] = params[:sap_key]
       flash[:notice] = 'ログインしました'
@@ -35,5 +39,11 @@ class Sap::SessionsController < Sap::ApplicationController
     session.delete(:student_id)
     session.delete(:sap_key)
     redirect_to(sap_signin_path+"?sap_key="+params[:sap_key])
+  end
+
+  private
+
+  def login_params
+    params.require(:sap_session).permit(:loginid, :password)
   end
 end
