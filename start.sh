3ab190a8c8fc13689f6490a5de7123cee4b93572
yusei-hyodo <yusei.hyodo@gmail.com>
2020-01-21 20:13:50 +0900

    for now deploy

--- start.sh
+++ start.sh
@@ -1,28 +1,28 @@
-# #!/bin/sh
-# RAILS_PORT=3000
-# if [ -n "$PORT" ]; then
-#   RAILS_PORT=$PORT
-# fi
+#!/bin/sh
+RAILS_PORT=3000
+if [ -n "$PORT" ]; then
+  RAILS_PORT=$PORT
+fi
 
-# # create database
-# #bundle exec rails db:create
+# create database
+bundle exec rails db:create
 
-# # migration
-# bundle exec rails db:migrate
+# migration
+bundle exec rails db:migrate
 
-# # precompile
-# bundle exec rake assets:precompile RAILS_ENV=production
+# precompile
+bundle exec rake assets:precompile RAILS_ENV=production
 
-# # Remove a potentially pre-existing server.pid for Rails.
-# rm -f tmp/pids/server.pid
+# Remove a potentially pre-existing server.pid for Rails.
+rm -f tmp/pids/server.pid
 
-# # cron
-# bundle exec whenever --update-crontab
 # cron
+bundle exec whenever --update-crontab
+cron
 
-# # sitemap
-# # send google and bing to ping
-# bundle exec rails sitemap:refresh
+# sitemap
+# send google and bing to ping
+bundle exec rails sitemap:refresh
 
-# # start rails app
-# bundle exec rails s -p $RAILS_PORT -b 0.0.0.0
+# start rails app
+bundle exec rails s -p $RAILS_PORT -b 0.0.0.0
