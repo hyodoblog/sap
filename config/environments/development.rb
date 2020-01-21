da22e7c2d61af8f1da5f789f37a77d4d56bc1218
yusei-hyodo <yusei.hyodo@gmail.com>
2020-01-21 19:46:54 +0900

    heroku docker

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,14 +60,14 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   # devise
-  config.action_mailer.default_url_options = { host: 'localhost' , port: 3000 }
+  config.action_mailer.default_url_options = { host: ENV['HOST'] ||= 'localhost' , port: ENV['PORT'] ||= 3000 }
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :enable_starttls_auto => true,
     :address => 'smtp.sendgrid.net',
     :port => 587,
-    :domain => 'localhost',
+    :domain => ENV['HOST'] ||= 'localhost',
     :user_name => ENV['SENDGRID_USER_NAME'],
     :password => ENV['SNEDGRID_PASSWORD'],
     :authentication => :plain
