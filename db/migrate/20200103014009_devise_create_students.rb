879dd9306761cacefee15588cdc9efaccec96ec5
yusei-hyodo <yusei.hyodo@gmail.com>
2020-02-19 04:48:00 +0900

    データベースの変更とシードフィアルを分割

--- db/migrate/20200103014009_devise_create_students.rb
+++ db/migrate/20200103014009_devise_create_students.rb
@@ -21,6 +21,13 @@ class DeviseCreateStudents < ActiveRecord::Migration[5.2]
       ## Rememberable
       t.datetime :remember_created_at
 
+      ## Trackable
+      t.integer  :sign_in_count, default: 0, null: false
+      t.datetime :current_sign_in_at
+      t.datetime :last_sign_in_at
+      t.string   :current_sign_in_ip
+      t.string   :last_sign_in_ip
+
       ## Lockable
       t.integer  :failed_attempts, default: 0, null: false # Only if lock strategy is :failed_attempts
       t.string   :unlock_token # Only if unlock strategy is :email or :both
--- db/schema.rb
+++ db/schema.rb
@@ -83,6 +83,11 @@ ActiveRecord::Schema.define(version: 2020_02_16_075512) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
     t.integer "failed_attempts", default: 0, null: false
     t.string "unlock_token"
     t.datetime "locked_at"
@@ -148,6 +153,11 @@ ActiveRecord::Schema.define(version: 2020_02_16_075512) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
     t.integer "failed_attempts", default: 0, null: false
     t.string "unlock_token"
     t.datetime "locked_at"
