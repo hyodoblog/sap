7eb146db937ffa2243070fae80ee5340b5552a84
yusei-hyodo <yusei.hyodo@gmail.com>
2020-02-19 05:41:39 +0900

    最終ログイン日の表示バグ修正

--- app/models/laboratory.rb
+++ app/models/laboratory.rb
@@ -2,7 +2,8 @@ class Laboratory < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :lockable
+         :recoverable, :rememberable, :lockable,
+         :trackable, :timeoutable
 
   belongs_to :admin,             optional: true
   has_many   :student_choice,    dependent: :destroy
