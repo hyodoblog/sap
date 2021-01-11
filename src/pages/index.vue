f6d342d56ce81146e3652a49f15aecd504722aad
yusei-hyodo <yusei.hyodo@gmail.com>
2021-01-12 02:19:30 +0900

    editnuxt.config

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
-    <img src="top.PNG">
-    <about/>
-    <service/>
-    <history/>
+    <img src="top.PNG" />
+    <about />
+    <service />
+    <history />
   </div>
 </template>
 
@@ -12,11 +12,11 @@ import About from '~/components/top/About.vue'
 import History from '~/components/top/History.vue'
 import Service from '~/components/top/Service.vue'
 export default {
-  //layout: 'auth',
+  // layout: 'auth',
   components: {
     About,
     Service,
     History,
-  }
+  },
 }
 </script>
