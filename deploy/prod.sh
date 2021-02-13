b6cc2dd81631dea7f5297a7b1a844f5ad1593e16
yusei-hyodo <yusei.hyodo@gmail.com>
2021-02-13 12:34:03 +0900

    command edit

--- deploy/prod.sh
+++ deploy/prod.sh
@@ -5,15 +5,30 @@ do
   export $env
 done < .env
 
-gcloud config set project ${FIREBASE_PROJECT_ID}
-docker build -t ${FIREBASE_PROJECT_ID}-prod .
-docker tag ${FIREBASE_PROJECT_ID}-prod gcr.io/${FIREBASE_PROJECT_ID}/prod
-docker push gcr.io/${FIREBASE_PROJECT_ID}/prod
-gcloud beta run deploy prod \
-  --image gcr.io/${FIREBASE_PROJECT_ID}/prod \
-  --max-instances 2 \
-  --region asia-northeast1 \
-  --platform managed \
-  --memory 1G \
-  --service-account ${GCLOUD_SERVICE_ACCOUNT_ID} \
-  --allow-unauthenticated
+# *******************
+# functions
+
+cloudFunctions () {
+  firebase functions:config:set store.env=${FIREBASE_STORE_ROOT_COLLECTION}
+  firebase deploy --only functions
+}
+
+# *******************
+# cloud run
+
+cloudRun () {
+  gcloud config set project ${FIREBASE_PROJECT_ID}
+  docker build -t ${FIREBASE_PROJECT_ID}-prod .
+  docker tag ${FIREBASE_PROJECT_ID}-prod gcr.io/${FIREBASE_PROJECT_ID}/prod
+  docker push gcr.io/${FIREBASE_PROJECT_ID}/prod
+  gcloud beta run deploy prod \
+    --image gcr.io/${FIREBASE_PROJECT_ID}/prod \
+    --max-instances 2 \
+    --region asia-northeast1 \
+    --platform managed \
+    --memory 1G \
+    --service-account ${GCLOUD_SERVICE_ACCOUNT_ID} \
+    --allow-unauthenticated
+}
+
+cloudFunctions & cloudRun
--- deploy/staging.sh
+++ deploy/staging.sh
@@ -5,15 +5,30 @@ do
   export $env
 done < .env
 
-gcloud config set project ${FIREBASE_PROJECT_ID}
-docker build -t ${FIREBASE_PROJECT_ID}-staging .
-docker tag ${FIREBASE_PROJECT_ID}-staging gcr.io/${FIREBASE_PROJECT_ID}/staging
-docker push gcr.io/${FIREBASE_PROJECT_ID}/staging
-gcloud beta run deploy staging \
-  --image gcr.io/${FIREBASE_PROJECT_ID}/staging \
-  --max-instances 2 \
-  --region asia-northeast1 \
-  --platform managed \
-  --memory 1G \
-  --service-account ${GCLOUD_SERVICE_ACCOUNT_ID} \
-  --allow-unauthenticated
+# *******************
+# functions
+
+cloudFunctions () {
+  firebase functions:config:set store.env=${FIREBASE_STORE_ROOT_COLLECTION}
+  firebase deploy --only functions
+}
+
+# *******************
+# cloud run
+
+cloudRun () {
+  gcloud config set project ${FIREBASE_PROJECT_ID}
+  docker build -t ${FIREBASE_PROJECT_ID}-staging .
+  docker tag ${FIREBASE_PROJECT_ID}-staging gcr.io/${FIREBASE_PROJECT_ID}/staging
+  docker push gcr.io/${FIREBASE_PROJECT_ID}/staging
+  gcloud beta run deploy staging \
+    --image gcr.io/${FIREBASE_PROJECT_ID}/staging \
+    --max-instances 2 \
+    --region asia-northeast1 \
+    --platform managed \
+    --memory 1G \
+    --service-account ${GCLOUD_SERVICE_ACCOUNT_ID} \
+    --allow-unauthenticated
+}
+
+cloudFunctions & cloudRun
